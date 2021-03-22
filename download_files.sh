92a586dd6265fd5953a09bab8c71e5413af915ea
aldenacabajar <aldenacabajar@gmail.com>
2021-03-22 15:43:08 +0800

    add download script

--- download_files.sh
+++ download_files.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-# make sure that you have your kaggle.json file in hand
+## make sure that you have your kaggle.json file in hand and that it is located in root!
+
+# install kaggle api, I would advise doing this in a python venv 
+pip install kaggle
+
+while getopts d: flag
+do
+	case "${flag}" in
+		d) dir=${OPTARG};;
+	esac
+
+done
+
+kaggle competitions download m5-forecasting-uncertainty -q
+
+
+
+
+
+
 
 
 
